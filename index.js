219fd4436ee31c16fed4b2a8d1a07bedae4d785e
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2026-05-17 22:34:33 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('WhatsApp Flow Server Running');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    version: "3.0",
+    screen": "SUCCESS",
+    data: {}
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
